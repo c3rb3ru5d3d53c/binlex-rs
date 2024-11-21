44f7844a946462f5b0b732c23fd5a110e828a304
c3rb3r3u5d3d53c <c3rb3ru5d3d53c@gmail.com>
2024-11-21 12:20:04 -0400

    Add LZ4String to Python Bindings

--- src/bindings/python/src/types/mod.rs
+++ src/bindings/python/src/types/mod.rs
@@ -1,8 +1,10 @@
 pub mod memorymappedfile;
+pub mod lz4string;
 
 use crate::types::memorymappedfile::memorymappedfile_init;
 
 pub use crate::types::memorymappedfile::MemoryMappedFile;
+pub use crate::types::lz4string::LZ4String;
 
 use pyo3::{prelude::*, wrap_pymodule};
 
@@ -11,6 +13,7 @@ use pyo3::{prelude::*, wrap_pymodule};
 pub fn types_init(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_wrapped(wrap_pymodule!(memorymappedfile_init))?;
     m.add_class::<MemoryMappedFile>()?;
+    m.add_class::<LZ4String>()?;
     py.import_bound("sys")?
         .getattr("modules")?
         .set_item("binlex.types", m)?;
